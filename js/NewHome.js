9b4cbafd1e2489922c19a5929cbe218e500c89af
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-11-27 17:20:59 +0530

    Added: Ability to view Employee Payroll details from Local Storage

--- js/NewHome.js
+++ js/NewHome.js
@@ -1,12 +1,20 @@
+let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
+    localStorage.removeItem("editEmp");
 });
 
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
+
 const createInnerHtml = () => {
+    if (empPayrollList.length == 0) return;
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
     let innerHtml = `${headerHtml}`;
-    let employeePayrollList = createEmployeePayrollJSON();
-    for (const empPayrollData of employeePayrollList) {
+    for (const empPayrollData of empPayrollList) {
         innerHtml= `${innerHtml}
         <tr>
             <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
@@ -15,14 +23,23 @@ const createInnerHtml = () => {
             <td>${getDeptHtml(empPayrollData._department)}</td>
             <td>${empPayrollData._salary}</td>
             <td>${empPayrollData._startDate}</td>
-            <td><img id="1" onclick="remove(this)" alt="delete" src="../assets/delete-black-18dp.svg">
-                <img id="1" onclick="update(this)" alt="edit" src="../assets/create-black-18dp.svg"></td>
+            <td><img name=${empPayrollData._id} onclick="remove(this)" alt="delete" src="../assets/delete-black-18dp.svg">
+                <img name=${empPayrollData._id} onclick="update(this)" alt="edit" src="../assets/create-black-18dp.svg"></td>
         </tr>
     `;
     }
     document.querySelector('#table-display').innerHTML = innerHtml;
 }
 
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
+
+/*
 const createEmployeePayrollJSON = () => {
     let empPayrollListLocal = [{
         _name: 'Ishani M',
@@ -51,12 +68,4 @@ const createEmployeePayrollJSON = () => {
     }
     ];
     return empPayrollListLocal;
-}
-
-const getDeptHtml = (deptList) => {
-    let deptHtml = '';
-    for (const dept of deptList) {
-        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
-    }
-    return deptHtml;
-}
+}*/
